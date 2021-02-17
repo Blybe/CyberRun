b48bf17ff6387f3f861a9291b7d6a5271eaeedd0
Blybe <58219313+Blybe@users.noreply.github.com>
2021-02-17 15:38:22 +0100

    Tilespawner

--- Assets/Scripts/TileSpawner.cs
+++ Assets/Scripts/TileSpawner.cs
@@ -15,7 +15,7 @@ public class TileSpawner : MonoBehaviour
         if (spawnTime >= spawnTimer)
         {
             spawnTime = 0f;
-            Instantiate(Tiles[Random.Range(0, Tiles.Length)], spawnLocation.position, spawnLocation.rotation);
+            Instantiate(Tiles[Random.Range(0, Tiles.Length)], new Vector3(20, spawnLocation.position.y, spawnLocation.position.z), spawnLocation.rotation);
         }
     }
 }
